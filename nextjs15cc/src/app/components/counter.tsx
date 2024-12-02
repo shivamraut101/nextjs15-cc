4dfd9eb72de9a057d8c24a2e23a8f7287469a312
shivamraut101 <shivamkumarraut12@gmail.com>
2024-12-03 00:04:03 +0530

    counter

---
+++ nextjs15cc/src/app/components/counter.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { useState } from "react";
+
+export const Counter = () =>{
+    console.log("Counter Components");
+    const [count, setCount] = useState(0);
+
+    return(
+        <>
+        <button onClick={()=> setCount(count +1)}> Clicked {count} times</button>
+        </>
+    )
+}
